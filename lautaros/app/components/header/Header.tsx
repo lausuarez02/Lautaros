492df7c138ce748d71b2452a79f0a95d15ef074e
Lautaro <lausuarez71@gmail.com>
2023-05-28 16:51:22 +0200

    Add new features to the nextjs website

---
+++ lautaros/app/components/header/Header.tsx
@@ -0,0 +1,12 @@
+//css
+import './Header.css'
+
+const Header = () => {
+    return(
+        <div className="main-header-container">
+            Header
+        </div>
+    )
+}
+
+export default Header;
---
+++ lautaros/app/views/Main.tsx
@@ -0,0 +1,7 @@
+import Header from "../components/header/Header";
+
+const Main = () => {
+    return <Header/>
+}
+
+export default Main;
